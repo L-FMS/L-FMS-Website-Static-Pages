bd527baa6d86d7dea41f749ae199ab143ca0eaf2
h1994st <h1994st@gmail.com>
2014-07-25 21:52:07 +0800

    init

---
+++ js/detail.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @authors Tom Hu (webmaster@h1994st.com)
+ * @date    2014-07-25 16:25:17
+ * @version 1.0
+ */
+
+$(document).ready(function() {
+  $('.item-comment textarea').focus(function(event) {
+    $('.item-comment .comment-btn').fadeIn(100);
+  });
+
+  $('.item-comment textarea').blur(function(event) {
+    $('.item-comment .comment-btn').fadeOut(100);
+  });
+});
